8fc73007cfb1eaa5932bf3297a55822c21af7401
abhishekk-s <abhi247997@gmail.com>
2024-02-15 16:59:18 +0530

    EP03

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
+// React Element
+const heading = (
+  <div>
+    <h1>Hello World from React Element</h1>
+  </div>
 );
 
-console.log(heading);  // Returns Object
+const AComponent = () => {
+  return <h1>AKS</h1>
+}
+
+const elem = (
+  <div>
+    {heading}
+    <h3>React Element</h3>
+    {AComponent()}
+    <AComponent />
+    <AComponent></AComponent>
+  </div>
+);
+
+const Title = () => (
+  <div>
+    <h1>AK</h1>
+  </div>
+);
+
+const number = 101;
+
+const HeadingComponent = () => (
+  <div>
+    {elem}
+    <h2>{number}</h2>
+    <Title />
+    <h1>Hello world from React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+//React Element rendering
+
+//root.render(heading);
+
+//React Component rendering
+root.render(<HeadingComponent />);
